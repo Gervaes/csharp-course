d04ee5f98d4e7b6bf0da8e2189b96105116e6726
Gervaes <eogervaes@gmail.com>
2022-02-17 17:44:59 -0300

    more progress

---
+++ exercicio_131/exercicio_131/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using exercicio_131.Entities;
+
+Console.Write("Enter the number of employees: ");
+int n = int.Parse(Console.ReadLine());
+
+List<Employee> employees = new List<Employee>();
+
+for (int i = 0; i < n; i++) {
+    Console.WriteLine($"Employee #{i+1} data:");
+    Console.Write("Outsourced (y/n)? ");
+    char outsourced = char.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Hours: ");
+    int hours = int.Parse(Console.ReadLine());
+    Console.Write("Value per hour: ");
+    double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    Employee emp;
+
+    if(outsourced == 'y') {
+        Console.Write("Additional Charge: ");
+        double addCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        emp = new OutsourcedEmployee(name, hours, valuePerHour, addCharge);
+    } else
+    {
+        emp = new Employee(name, hours, valuePerHour);//
+    }
+
+    employees.Add(emp);
+}
+
+Console.WriteLine();
+Console.WriteLine("PAYMENTS");
+
+foreach (Employee employee in employees) {
+    Console.WriteLine($"{employee.Name} - ${employee.Payment().ToString("F2", CultureInfo.InvariantCulture)}");
+}
---
+++ exercicio_133/exercicio_133/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using exercicio_133.Entities;
+
+Console.Write("Enter the number of products: ");
+int n = int.Parse(Console.ReadLine());
+
+List<Product> products = new List<Product>();
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Product #{i + 1} data:");
+    Console.Write("Common, used or imported (c/u/i)? ");
+    char type = char.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Price: ");
+    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    Product product;
+
+    if (type == 'i') {
+
+        Console.Write("Customs fee: ");
+        double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        product = new ImportedProduct(name, price, customsFee);
+    }
+    else if (type == 'u') {
+
+        Console.Write("Manufacture date: ");
+        DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+
+        product = new UsedProduct(name, price, manufactureDate);
+    } else {
+        product = new Product(name, price);
+    }
+
+    products.Add(product);
+}
+
+Console.WriteLine();
+Console.WriteLine("PRICE TAGS:");
+
+foreach (Product product in products) {
+    Console.WriteLine(product.PriceTag());
+}
